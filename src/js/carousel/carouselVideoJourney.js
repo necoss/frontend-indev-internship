e8c2eadd978a65d3f842b0d1142a3e413c052a3a
trexi <vartypeez@gmail.com>
2025-07-16 12:05:49 +0300

    Replaced plug carousel in Video journey section with the working one

---
+++ src/js/carousel/carouselVideoJourney.js
@@ -0,0 +1,32 @@
+import { addPrevNextBtnsClickHandlers } from './EmblaCarouselArrowButton.js'
+import { addDotBtnsAndClickHandlers } from './EmblaCarouselDotButton.js'
+
+// localStorage.clear()
+
+const OPTIONS = { align: 'start', loop:true }
+
+const emblaNode = document.querySelector('.embla_vj')
+const viewportNode = emblaNode.querySelector('.embla_vj__viewport')
+const prevBtnNode = emblaNode.querySelector('.embla_vj__button--prev')
+const nextBtnNode = emblaNode.querySelector('.embla_vj__button--next')
+const dotsNode = emblaNode.querySelector('.embla_vj__dots')
+
+console.log(viewportNode)
+
+const emblaApi = EmblaCarousel(viewportNode, OPTIONS)
+
+const removeClickHandlers = () => {
+
+	const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
+		emblaApi,
+		prevBtnNode,
+		nextBtnNode,
+		// onNavButtonClick
+	)
+	const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(
+		emblaApi,
+		dotsNode
+	)
+}
+
+emblaApi.on('destroy', removeClickHandlers())
