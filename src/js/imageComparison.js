fb22fe22cb5669f3f1d210a64df98ef5f1d21e8b
trexi <vartypeez@gmail.com>
2025-07-17 11:17:50 +0300

    Removed flag from map in Contacts section & Changed Image Compare Viever to native without libs

--- src/js/imageComparison.js
+++ src/js/imageComparison.js
@@ -1,13 +1,40 @@
-const element = document.getElementById('image-compare')
+//! REALIZATION USING IMAGE COMPARE VIEWER
+// const element = document.getElementById('image-compare')
 
-const options = {
-  controlColor: "#ffffff",
-  controlShadow: false,
-  addCircle: true,
-  addCircleBlur: true,
+// const options = {
+//   controlColor: "#ffffff",
+//   controlShadow: false,
+//   addCircle: true,
+//   addCircleBlur: true,
+// }
+
+// const viewer = new ImageCompare(element, options).mount()
+
+// function imageComparator() {
+//   sizeDiv = $('#slider-ic').val();
+//   $('.image-original').css('width',sizeDiv + '%');
+//   $('#cursor-ic').css('left',sizeDiv + '%');
+// }
+
+// $('#slider-ic').on('mousemove touchmove click', function(){
+//   imageComparator();
+// });
+
+function imageComparator() {
+  const sizaDiv = document.getElementById('slider-ic').value
+  const imageOriginal = document.querySelector('.image-original')
+  const cursorIc = document.getElementById('cursor-ic')
+
+  imageOriginal.style.width = sizaDiv + '%'
+  cursorIc.style.left = sizaDiv + '%'
 }
 
-const viewer = new ImageCompare(element, options).mount()
+const callImageComporatorFunc = () => {
+  imageComparator()
+}
 
-console.log(1)
+document.getElementById('slider-ic').addEventListener('click', () => imageComparator())
 
+document.getElementById('slider-ic').addEventListener('touchmove', () => imageComparator())
+
+document.getElementById('slider-ic').addEventListener('mousemove', () => imageComparator())
