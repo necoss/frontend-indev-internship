af88f541a1f8f8b28fb459c18f6ef691a3096f9c
trexi <vartypeez@gmail.com>
2025-07-27 18:36:57 +0300

    Fixed some issues

--- app.js
+++ app.js
@@ -1,135 +1,135 @@
-const body = document.getElementById('body')
-const div = document.createElement('div')
-const ul = document.createElement('ul')
-const li = document.createElement('li')
+document.addEventListener('DOMContentLoaded', () => {
+  const body = window.document.getElementById('body')
+  const div = window.document.createElement('div')
+  const ul = window.document.createElement('ul')
+  const list = ul
+  const li = window.document.createElement('li')
 
-generateDivControls()
-setupPage()
+  setupPage()
+  getUserVisitsCount()
+  addMetaTag()
+  generateDivControls()
 
-document.addEventListener('DOMContentLoaded', getUserVisitsQuantity)
+  function setupPage() {
+    setElement(body, list)
+    setElementClassName(list, 'list')
 
-function generateDivControls() {
-  const divControls = document.createElement('div')
-  const buttonAdd = document.createElement('button')
-  const buttonDelete = document.createElement('button')
-  const buttonDuplicate = document.createElement('button')
-  const buttonList = document.createElement('button')
-  const buttonPushState = document.createElement('button')
-  const buttonUserAgent = document.createElement('button')
+    list.addEventListener('click', changeElementStyle)
 
-  setElement(body, divControls)
-  divControls.id = 'controls'
-
-  buttonAdd.insertAdjacentHTML('beforeend', 'Добавить')
-  divControls.append(buttonAdd)
-  buttonDelete.insertAdjacentHTML('beforeend', 'Удалить Последний')
-  divControls.append(buttonDelete)
-  buttonDuplicate.insertAdjacentHTML('beforeend', 'Дублировать существующие')
-  divControls.append(buttonDuplicate)
-  buttonList.insertAdjacentHTML('beforeend', 'Состав списка')
-  divControls.append(buttonList)
-  buttonPushState.insertAdjacentHTML('beforeend', 'Пушнуть состояне')
-  divControls.append(buttonPushState)
-  buttonUserAgent.insertAdjacentHTML('beforeend', 'Юзер Агент')
-  divControls.append(buttonUserAgent)
-
-  buttonAdd.addEventListener('click', addElementToList)
-  buttonDelete.addEventListener('click', deleteElementFromList)
-  buttonDuplicate.addEventListener('click', duplicateList)
-  ul.addEventListener('click', changeElementStyle)
-  buttonList.addEventListener('click', inspectCollection)
-  buttonPushState.addEventListener('click', pushState)
-  buttonUserAgent.addEventListener('click', getUserAgent)
-  document.addEventListener('DOMContentLoaded', addMetaTag)
-}
-
-function setupPage() {
-  setElement(body, div)
-  setElement(div, ul)
-
-  setElementClassName(ul, 'list')
-  fillList(ul)
-
-  window.list = ul
-}
-
-function getUserVisitsQuantity() {
-  let count = Number(localStorage.getItem('visitCount')) || 0
-  localStorage.setItem('visitCount', count + 1)
-  // alert(`You visited this webpage ${count + 1} times`)
-}
-
-function addElementToList() {
-  const newLi = document.createElement('li')
-  newLi.insertAdjacentHTML('beforeend', `This is element #${list.getElementsByTagName('li').length + 1}`)
-  newLi.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16)
-  ul.append(newLi)
-}
-
-function deleteElementFromList() {
-  if (list.getElementsByTagName('li').length >= 1) {
-    const lastElem = list.getElementsByTagName('li').length - 1
-    list.getElementsByTagName('li')[lastElem].remove()
-  }
-}
-
-function duplicateList() {
-  const clone = ul.cloneNode(true)
-  div.append(clone)
-}
-
-function changeElementStyle(element) {
-  element.target.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16)
-
-  if (element.target.style.fontSize <= '16px') {
-    element.target.style.fontSize = '20px'
-  } else {
-    element.target.style.fontSize = '10px'
-  }
-}
-
-function inspectCollection() {
-  let elements = ''
-  const elementsQuantity = list.getElementsByTagName('li').length
-
-  for (let i = 0; i < list.getElementsByTagName('li').length; i++) {
-    elements += list.getElementsByTagName('li')[i].innerText + ', '
+    let createElementsCount = 0
+    while (createElementsCount < 5) {
+      const fillingElement = document.createElement('li')
+      fillingElement.style.fontSize = '16px'
+      list.appendChild(fillingElement)
+      fillingElement.textContent = `This is element #${createElementsCount + 1}`
+      fillingElement.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16)
+      createElementsCount++
+    }
   }
 
-  alert(`There are ${elementsQuantity} elements: ${elements}`)
-}
+  function generateDivControls() {
+    const divControls = document.createElement('div')
+    const buttonAdd = document.createElement('button')
+    const buttonDelete = document.createElement('button')
+    const buttonDuplicate = document.createElement('button')
+    const buttonShowList = document.createElement('button')
+    const buttonPushState = document.createElement('button')
+    const buttonUserAgent = document.createElement('button')
 
-function pushState() {
-  history.pushState({}, '?page=<rand>', '?page=<pipipu>')
-  const currentUrl = window.location.href
-  alert(`Your location: ${currentUrl}`)
-}
+    setElement(body, divControls)
+    divControls.id = 'controls'
 
-function getUserAgent() {
-  alert(navigator.userAgent) //Интересный факт, но я писал генератор фейковых (но рабочих) юзер агентов (им пользуются)
-}
+    buttonAdd.textContent = 'Добавить'
+    buttonAdd.addEventListener('click', addElementToList)
 
-function addMetaTag() {
-  const meta = document.createElement('meta')
-  meta.name = 'description'
-  meta.content = 'Mini-DOM-Playground training app'
-  document.getElementsByTagName('head')[0].appendChild(meta)
-}
+    buttonDelete.textContent = 'Удалить Последний'
+    buttonDelete.addEventListener('click', deleteElementFromList)
 
-function setElement(setTo, elementToSet) {
-  setTo.append(elementToSet)
-}
+    buttonDuplicate.textContent = 'Дублировать существующие'
+    buttonDuplicate.addEventListener('click', duplicateList)
 
-function setElementClassName(element, className) {
-  element.className = className
-}
+    buttonShowList.textContent = 'Состав списка'
+    buttonShowList.addEventListener('click', showCollection)
 
-function fillList(elemToFill) {
-  for (let i = 1; i <= 5; i++) {
-    elemFillWith = document.createElement('li')
-    elemFillWith.style.fontSize = '16px'
-    elemToFill.appendChild(elemFillWith)
-    elemFillWith.insertAdjacentHTML('beforeend', `This is element #${i}`)
-    elemFillWith.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16)
+    buttonPushState.textContent = 'Пушнуть состояне'
+    buttonPushState.addEventListener('click', pushState)
+
+    buttonUserAgent.textContent = 'Юзер Агент'
+    buttonUserAgent.addEventListener('click', getUserAgent)
+
+    divControls.append(buttonAdd)
+    divControls.append(buttonDelete)
+    divControls.append(buttonDuplicate)
+    divControls.append(buttonShowList)
+    divControls.append(buttonPushState)
+    divControls.append(buttonUserAgent)
   }
-}
+
+  function getUserVisitsCount() {
+    const parsedCount = localStorage.getItem('visitCount')
+    let count = Number(JSON.parse(parsedCount)) || 0
+    window.localStorage.setItem('visitCount', JSON.stringify(count + 1))
+    // alert(`You visited this webpage ${count + 1} times`)
+  }
+
+  function addElementToList() {
+    const newLi = document.createElement('li')
+    newLi.textContent = `This is element #${list.getElementsByTagName('li').length + 1}`
+    newLi.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16)
+    list.append(newLi)
+  }
+
+  function deleteElementFromList() {
+    if (list.getElementsByTagName('li').length >= 1) {
+      const lastElem = list.getElementsByTagName('li').length - 1
+      list.getElementsByTagName('li')[lastElem].remove()
+    }
+  }
+
+  function duplicateList() {
+    const clone = list.cloneNode(true)
+    div.append(clone)
+  }
+
+  function changeElementStyle(event) {
+    event.target.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16)
+
+    if (event.target.style.fontSize <= '16px') {
+      event.target.style.fontSize = '20px'
+    } else {
+      event.target.style.fontSize = '10px'
+    }
+  }
+
+  function showCollection() {
+    const elementsCount = list.childElementCount
+    const elementsText = [...list.children].map(element => element.innerText)
+
+    alert(`There are ${elementsCount} elements: ${elementsText.join(', ')}`)
+  }
+
+  function pushState() {
+    history.pushState({}, '?page=<rand>', '?page=<pipipu>')
+    const currentUrl = window.location.href
+    alert(`Your location: ${currentUrl}`)
+  }
+
+  function getUserAgent() {
+    alert(navigator.userAgent)
+  }
+
+  function addMetaTag() {
+    const meta = document.createElement('meta')
+    meta.name = 'description'
+    meta.content = 'Mini-DOM-Playground training app'
+    document.getElementsByTagName('head')[0].appendChild(meta)
+  }
+
+  function setElement(setTo, elementToSet) {
+    setTo.append(elementToSet)
+  }
+
+  function setElementClassName(element, className) {
+    element.className = className
+  }
+})
